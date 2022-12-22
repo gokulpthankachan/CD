117a574a20fc4cfe0b73592c7b25e111a75e9b09
GOKU <75922718+gokulpthankachan@users.noreply.github.com>
2022-12-22 09:56:18 +0000

    Update arith_rdp.c

--- Cycle 3/arith_rdp.c
+++ Cycle 3/arith_rdp.c
@@ -17,8 +17,8 @@ int i = 0;
 
 int E(char str[100])
 {
-    if(T(str)==1)
-        if(EPrime(str)==1)
+    if(T(str))
+        if(EPrime(str))
             return 1;
         return 0;
     return 0; 
@@ -29,8 +29,8 @@ int EPrime(char str[100])
     if (str[i] == '+' || str[i] == '-')
     {
         i++;
-        if(T(str)==1)
-            if(EPrime(str)==1)
+        if(T(str))
+            if(EPrime(str))
                 return 1;
             return 0;
         return 0; 
@@ -43,8 +43,8 @@ int EPrime(char str[100])
 
 int T(char str[100])
 {
-    if(F(str)==1)
-        if(TPrime(str)==1)
+    if(F(str))
+        if(TPrime(str))
             return 1;
         return 0;
     return 0;
@@ -55,8 +55,8 @@ int TPrime(char str[100])
     if (str[i] == '*' || str[i] == '/')
     {
         i++;
-        if(F(str)==1)
-            if(TPrime(str)==1)
+        if(F(str))
+            if(TPrime(str))
                 return 1;
             return 0;
         return 0;
@@ -85,7 +85,7 @@ int F(char str[100])
         i++;
         return 1;
     }
-    else if(isdigit(str[i]) == 1)
+    else if(isdigit(str[i]))
     {
 		while(isdigit(str[i]))
 			i++;
